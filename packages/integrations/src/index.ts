406d4f979791204e210bd0eef1553aa9f81efc70
Dominator0311 <abhinavg707@gmail.com>
2026-07-10 20:08:50 +0100

    fix(cp14): expose private media integration contracts

--- packages/integrations/src/index.ts
+++ packages/integrations/src/index.ts
@@ -1,4 +1,5 @@
 export * from "./ai-provider.js";
+export * from "./media/index.js";
 export * from "./messaging-provider.js";
 export * from "./payment-provider.js";
 export * from "./provider-contracts.js";
